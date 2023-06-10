56bb6ac36e3a7293ae73b5ed608c05d23d9f12ac
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2023-06-10 11:57:19 -0300

    criação do banner da aplicação

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <template>
+  <Banner></Banner>
   <div class="main-container">
-      <h1>HOME</h1>
+      <h1>Monte seu burger</h1>
+      <p>Formulário</p>
   </div>
 </template>
 
 <script>
+import Banner from '@/components/Banner.vue';
+
   export default {
     name: 'HomeView',
     components: {
+      Banner,
     }
   }
 </script>
