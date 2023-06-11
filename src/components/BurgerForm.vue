4b3537205635f5597c70a256f9101d7137624a10
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2023-06-11 15:59:40 -0300

    resgatando dados do back-end

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -9,22 +9,22 @@
             <div class="input-container">
                 <label for="pao">Escolha o pão:</label>
                 <select name="pao" id="pao" v-model="pao">
-                    <option value="">Selecione o seu pão</option>
-                    <option value="integral">Integral</option>
+                    <option value="default" disabled>Selecione o seu pão</option>
+                    <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{pao.tipo}}</option>
                 </select>
             </div>
             <div class="input-container">
                 <label for="carne">Escolha a carne do seu Burger:</label>
                 <select name="carne" id="carne" v-model="carne">
-                    <option value="">Selecione o tipo de carne</option>
-                    <option value="maminha">Maminha</option>
+                    <option value="" disabled>Selecione o tipo de carne</option>
+                    <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{carne.tipo}}</option>
                 </select>
             </div>
             <div id="opcionais-container" class="input-container">
                 <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                    <span>Salame</span>
+                <div v-for="opcional in opcionaisData" :key="opcional.id" class="checkbox-container">
+                    <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                    <span>{{opcional.tipo}}</span>
                 </div>
             </div>
             <div class="input-container">
@@ -39,10 +39,27 @@ export default {
     name: 'BurgerForm',
     data(){
         return{
-            nome: '',
-            pao: String,
-            carne: String,
-            opcionais: String,
+            paes: null,
+            carnes: null,
+            opcionaisData: null,
+            nome: null,
+            pao: null,
+            carne: null,
+            opcionais: [],
+            status: "Solicitado",
+            msg: null
+        }
+    },
+    mounted(){
+        this.getIngredientes();
+    },
+    methods: {
+        async getIngredientes(){
+            const req = await fetch('http://localhost:3000/ingredientes');
+            const data = await req.json();
+            this.paes = data.paes;
+            this.carnes = data.carnes;
+            this.opcionaisData = data.opcionais;
         }
     }
 }
