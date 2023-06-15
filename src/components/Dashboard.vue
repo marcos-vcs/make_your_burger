05d83926b40ad3e3fa41193f23113b2c1960bb4f
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2023-06-15 19:54:30 -0300

    trazendo dados do componente dashboard de forma dinamica

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,15 +11,14 @@
                     <div>Ações:</div>
                 </div>
                 <div id="burger-table-rows">
-                    <div class="burger-table-row">
-                        <div class="order-number">1</div>
-                        <div>João</div>
-                        <div>Pão de Trigo</div>
-                        <div>Maminha</div>
+                    <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                        <div class="order-number">{{burger.id}}</div>
+                        <div>{{burger.nome}}</div>
+                        <div>{{burger.pao}}</div>
+                        <div>{{burger.carne}}</div>
                         <div>
                             <ul>
-                                <li>Salame</li>
-                                <li>Tomate</li>
+                                <li v-for="(opcional, index) in burger.opcionais" :key="index">{{opcional}}</li>
                             </ul>
                         </div>
                         <div>
@@ -38,7 +37,26 @@
 <script>
 export default {
     name: 'Dashboard',
+    data(){
+        return {
+            burgers: null,
+            burger_id: null,
+            status: [],
+        }
+    },
+    methods: {
+        async getPedidos(){
+            const req = await fetch('http://localhost:3000/burgers');
+            const data = await req.json();
+            console.log(data);
+            this.burgers = data;
 
+            // resgatando os status
+        },
+    },
+    mounted(){
+        this.getPedidos();
+    }
 }
 </script>
 
